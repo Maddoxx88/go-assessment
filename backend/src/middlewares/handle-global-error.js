ccae256708c10c67359f238f6874e2d4f90fbcea
Sunit Shirke <sunitshirke88@gmail.com>
2025-08-08 14:03:25 -0400

    err: handle global error and log it out

--- backend/src/middlewares/handle-global-error.js
+++ backend/src/middlewares/handle-global-error.js
@@ -1,12 +1,15 @@
 const { ApiError } = require("../utils");
 
 const handleGlobalError = (err, req, res, next) => {
-    console.error(err);
-    if (err instanceof ApiError) {
-        return res.status(err.statusCode).json({ error: err.message });
-    }
+  console.error("🔥 Global error handler caught an error:");
+  console.error(err); // will show the whole object in terminal
 
-    return res.status(500).json({ error: "Internal server error" });
-}
+  const statusCode = err.status || 500;
+
+  res.status(statusCode).json({
+    error: err.message || "Unknown server error",
+    detail: err.stack || err, // show raw error if no stack
+  });
+};
 
 module.exports = { handleGlobalError };
