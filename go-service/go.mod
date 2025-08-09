00832472ff6b784f7639210301a1f6c4daa0dfda
Sunit Shirke <sunitshirke88@gmail.com>
2025-08-09 00:10:32 -0400

    feat: init token at startup of app

--- go-service/go.mod
+++ go-service/go.mod
@@ -3,6 +3,6 @@ module go-service
 go 1.22.0
 
 require (
-	github.com/gorilla/mux v1.8.1 // indirect
-	github.com/jung-kurt/gofpdf v1.16.2 // indirect
+	github.com/gorilla/mux v1.8.1
+	github.com/jung-kurt/gofpdf v1.16.2
 )
--- go-service/main.go
+++ go-service/main.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"fmt"
+	"go-service/utils"
 	"log"
 	"net/http"
 )
 
 func main() {
-	r := InitializeRouter() // From router.go
-	log.Println("PDF microservice running on :8080")
+	r := InitializeRouter()
+	// Login at startup and get tokens map
+	tokens, err := utils.LoginAndGetTokens()
+	if err != nil {
+		log.Fatalf("Failed to login at startup: %v", err)
+	}
+
+	// Set tokens in utils package for later use
+	utils.SetTokens(tokens["accessToken"], tokens["refreshToken"], tokens["csrfToken"])
+	fmt.Println("Logged in and tokens stored at startup")
+
+	log.Println("Go service started on :8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
