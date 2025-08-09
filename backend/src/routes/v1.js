adf27a6d4c56874cf09e93d4e3e0bb1f80c0f7b5
Sunit Shirke <sunitshirke88@gmail.com>
2025-08-08 22:40:00 -0400

    fix: bypass department routes due to module handler

--- backend/src/routes/v1.js
+++ backend/src/routes/v1.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 const { authenticateToken, handle404Error, csrfProtection, checkApiAccess } = require("../middlewares");
+
+// Comment out ALL module imports temporarily
 const { studentsRoutes } = require("../modules/students/sudents-router.js");
 const { authRoutes } = require("../modules/auth/auth-router.js");
 const { rpRoutes } = require("../modules/roles-and-permissions/rp-router.js");
@@ -13,10 +15,11 @@ const { handleGetAllTeachers } = require("../modules/class-teacher/class-teacher
 const { staffsRoutes } = require("../modules/staffs/staffs-router.js");
 const { accountRoutes } = require("../modules/account/account-router.js");
 const { sectionRoutes } = require("../modules/sections/section-router.js");
-const { departmentRoutes } = require("../modules/departments/department-router.js");
+// const { departmentRoutes } = require("../modules/departments/department-router.js");
 const { handleGetDashboardData } = require("../modules/dashboard/dashboard-controller.js");
 const { accessControlRoutes } = require("../modules/access-control/access-control-router.js");
 
+// Comment out all route uses
 router.get("/teachers", authenticateToken, csrfProtection, checkApiAccess, handleGetAllTeachers);
 router.get("/dashboard", authenticateToken, csrfProtection, checkApiAccess, handleGetDashboardData);
 router.use("/access-controls", authenticateToken, csrfProtection, accessControlRoutes);
@@ -29,8 +32,9 @@ router.use("/sections", authenticateToken, csrfProtection, sectionRoutes);
 router.use("/students", authenticateToken, csrfProtection, studentsRoutes);
 router.use("/notices", authenticateToken, csrfProtection, noticesRoutes);
 router.use("/staffs", authenticateToken, csrfProtection, staffsRoutes);
-router.use("/departments", authenticateToken, csrfProtection, departmentRoutes);
+// router.use("/departments", authenticateToken, csrfProtection, departmentRoutes);
 router.use("/roles", authenticateToken, csrfProtection, rpRoutes);
+
 router.use(handle404Error);
 
 module.exports = { v1Routes: router };
